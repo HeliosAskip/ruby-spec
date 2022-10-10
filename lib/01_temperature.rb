75508ea5b8d3e2db519174daf74a5c9be8ec949c
Romain ABIDOS <romain.abidos.lyon@gmail.com>
2022-10-10 19:10:03 +0200

    voilà

---
+++ lib/01_temperature.rb
@@ -0,0 +1,8 @@
+
+def ftoc(temperature)
+   ( temperature - 32 ) * 5/9
+end
+
+def ctof(temp)
+  ( temp * 9.0/5 ) + 32
+end
---
+++ lib/02_calculator.rb
@@ -0,0 +1,15 @@
+def add(a, b)
+  return a + b
+end
+
+def subtract(a, b)
+  return a - b
+end
+
+def sum(array)
+  return array.sum
+end
+
+def multiply(a, b)
+  return a * b
+end
